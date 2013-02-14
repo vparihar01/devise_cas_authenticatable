7d12bb1a8787f7d1bc7b3bdc65e29fb3ba7b24fa
Vivek Parihar <vivek@weboniselab.com>
2013-02-14 13:01:02 +0530

    sesstion store error removed

--- lib/devise_cas_authenticatable/single_sign_out/session_store/active_record.rb
+++ lib/devise_cas_authenticatable/single_sign_out/session_store/active_record.rb
@@ -4,7 +4,7 @@ ActiveRecord::SessionStore.class_eval do
   alias_method_chain :set_session, :storage
 
   def destroy_session(env, session_id, options)
-    if session = Session::find_by_session_id(sid)
+    if session = ActiveRecord::SessionStore.session_class.find_by_session_id(sid)
       session.destroy
     end
   end
